9643310312776fbc64ca95910e146f5274a7fa66
miftah izharuddin <61154976+miftah06@users.noreply.github.com>
2024-06-04 07:17:06 +0800

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import subprocess
+import shutil
+import os
 
 # Fungsi untuk menjalankan skrip dan membuat unit systemd
 def install_and_create_systemd_unit(script_name, domain=None):
-    # Jalankan skrip dengan subprocess
-    subprocess.run(["chmod", "+x", script_name])  # Berikan izin eksekusi pada skrip
-    if domain:
-        subprocess.run(["./" + script_name, domain])
-    else:
-        subprocess.run(["./" + script_name])
+    # Tentukan jalur tujuan untuk skrip
+    destination = "/usr/bin/" + script_name
 
+    # Pindahkan skrip jika itu adalah file .sh
+    if script_name.endswith(".sh"):
+        # Berikan izin eksekusi pada skrip sebelum memindahkannya
+        subprocess.run(["chmod", "+x", script_name])
+        if not os.path.exists(destination):
+            shutil.move(script_name, destination)
+
+    # Tentukan jalur eksekusi berdasarkan apakah skrip adalah file .sh atau tidak
+    exec_start = destination if script_name.endswith(".sh") else f"/usr/bin/python3 /usr/bin/{script_name} {domain}"
+    
     # Buat unit systemd untuk skrip yang dijalankan
     service_name = script_name.split('.')[0] + ".service"
     with open(f"/etc/systemd/system/{service_name}", "w") as unit_file:
@@ -19,7 +27,7 @@ Description=Service for {script_name}
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 {script_name} {domain}
+ExecStart={exec_start}
 Restart=always
 
 [Install]
